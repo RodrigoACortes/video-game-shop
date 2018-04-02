a5057b0de44c4f30b3bea3afef4887f87c0b1147
YOUR NAME <rodrigocortes98@live.com>
2018-04-02 14:38:29 -0400

    added billing and client side app

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>
+                Shop!
+            </h1>
+            Buy Stuff
+        </div>
+    )
+}
+
+export default Landing
