dd35e5110277b83504aefa250a1a63e8ce8cd9a6
YOUR NAME <rodrigocortes98@live.com>
2018-04-02 16:39:32 -0400

    trying to debug

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -19,6 +19,8 @@ module.exports = app => {
   app.get("/api/logout", (req, res) => {
     req.logout();
     res.redirect('/');
+    console.log(res);
+    
   });
 
   app.get("/api/current_user", (req, res) => {
